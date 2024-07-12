e87ef9480cb69efaada7ac53627d378931404f3a
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2024-07-12 12:01:25 +0200

    add CU

--- umi/index.ts
+++ umi/index.ts
@@ -1,57 +1,62 @@
-import { 
-    mplHydra, 
-    Fanout, 
-    fetchFanout, 
-    findFanoutNativeAccountPda, 
-    findFanoutPda, 
-    init, 
-    MembershipModel
- } from "@metaplex-foundation/mpl-hydra";
-import { generateRandomString, publicKey, generateSigner, none, keypairIdentity, Context, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi";
-import { createUmi as baseCreateUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import { clusterApiUrl } from "@solana/web3.js";
+import {
+  mplHydra,
+  Fanout,
+  fetchFanout,
+  findFanoutNativeAccountPda,
+  findFanoutPda,
+  init,
+  MembershipModel,
+} from "@metaplex-foundation/mpl-hydra";
+import {
+  generateRandomString,
+  publicKey,
+  generateSigner,
+  none,
+  keypairIdentity,
+  Context,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  setComputeUnitPrice,
+  setComputeUnitLimit,
+} from "@metaplex-foundation/mpl-toolbox";
 import base58 from "bs58";
 
-
-// secret key base_58 
+// secret key base_58
 const SK_B58 = "";
 
 (async () => {
-  
-    // const createUmi = async () => (await baseCreateUmi(`https://mainnet.helius-rpc.com/?api-key=${API_KEY}`)).use(mplHydra());
+  const umi = createUmi("<endpoint>").use(mplHydra());
 
-    const createUmi = async () => (await baseCreateUmi(clusterApiUrl("devnet"))).use(mplHydra());
+  const name = generateRandomString();
 
-    const umi = await createUmi();
-    const name = generateRandomString();
-  
-    const secretKey = base58.decode(SK_B58)
+  const secretKey = base58.decode(SK_B58);
 
+  const keypair = umi.eddsa.createKeypairFromSecretKey(secretKey);
+  const signer = createSignerFromKeypair(umi, keypair);
 
-    const keypair = umi.eddsa.createKeypairFromSecretKey(secretKey);
-    const signer = createSignerFromKeypair(umi, keypair);
-    umi.identity = signer;
-    umi.payer = signer;
-    
-    umi.use(keypairIdentity(signer, true))
+  umi.use(keypairIdentity(signer));
 
-    const wallet = await init(umi, {
-        name,
-        model: MembershipModel.Wallet,
-        totalShares: 100,
-      }).sendAndConfirm(umi);
-    
-    
-    console.log(base58.encode(wallet.signature));
+  let tx = await init(umi, {
+    name,
+    model: MembershipModel.Wallet,
+    totalShares: 100,
+  })
+    .prepend(setComputeUnitPrice(umi, { microLamports: 50000 }))
+    .prepend(setComputeUnitLimit(umi, { units: 50_000 }))
+    .sendAndConfirm(umi);
 
+  console.log(base58.encode(tx.signature));
 
-    // const [fanout, fanoutBump] = findFanoutPda(umi, { name });
-    // console.log(fanout, fanoutBump);
+  // const [fanout, fanoutBump] = findFanoutPda(umi, { name });
+  // console.log(fanout, fanoutBump);
 
-    // console.log(umi);
+  // console.log(umi);
 
-    // const k = generateSigner(umi).publicKey;
-    // const k = umi.eddsa.generateKeypair()
+  // const k = generateSigner(umi).publicKey;
+  // const k = umi.eddsa.generateKeypair()
 
-    // console.log(k)
+  // console.log(k)
 })();
